bf9c432bac97d2a3a0ceaa1403f002d046bbfc56
Jens Segers <segers.jens@gmail.com>
2019-01-04 11:14:21 +0100

    :fire: Remove duplicate test

--- tests/OptimusTest.php
+++ tests/OptimusTest.php
@@ -21,96 +21,6 @@ class OptimusTest extends TestCase
     }
 
     /**
-     * @dataProvider getPrimesTestData
-     */
-    public function testEncodeDecode(int $prime, int $inverse, int $xor, int $bitLength, int $value)
-    {
-        $optimus = new Optimus($prime, $inverse, $xor, $bitLength);
-
-        $encoded = $optimus->encode($value);
-        $decoded = $optimus->decode($encoded);
-
-        $assertMsgDetails = sprintf(
-            'Prime: %s, Inverse: %s, Xor: %s, Bit length: %s, Value: %s',
-            $prime,
-            $inverse,
-            $xor,
-            $bitLength,
-            $value
-        );
-
-        $this->assertNotEquals(
-            $value,
-            $encoded,
-            "The encoded value is not different to the original value. ($assertMsgDetails)"
-        );
-        $this->assertNotEquals(
-            $encoded,
-            $decoded,
-            "The encoded and decoded values are equal. ($assertMsgDetails)"
-        );
-        $this->assertEquals(
-            $value,
-            $decoded,
-            "The encoded value did not decode correctly. ($assertMsgDetails)"
-        );
-    }
-
-    public function getPrimesTestData(): array
-    {
-        $bitLength31 = 31;
-        $bitLength32 = 32;
-        $bitLength24 = 24;
-
-        $bitLengths = [
-            $bitLength31,
-            $bitLength32,
-            $bitLength24,
-        ];
-
-        $randXor = 873691988;
-        $smlPrime = 10000019;
-
-        $lrgPrimes = [
-            $bitLength31 => 2147483647,
-            $bitLength32 => 4294967291,
-            $bitLength24 => 999999967,
-        ];
-
-        $smlPrimeInverses = [
-            $bitLength31 => Energon::calculateInverse($smlPrime, $bitLength31),
-            $bitLength32 => Energon::calculateInverse($smlPrime, $bitLength32),
-            $bitLength24 => Energon::calculateInverse($smlPrime, $bitLength24),
-        ];
-
-        $lrgPrimeInverses = [
-            $bitLength31 => Energon::calculateInverse($lrgPrimes[$bitLength31], $bitLength31),
-            $bitLength32 => Energon::calculateInverse($lrgPrimes[$bitLength32], $bitLength32),
-            $bitLength24 => Energon::calculateInverse($lrgPrimes[$bitLength24], $bitLength24),
-        ];
-
-        $testData = [];
-
-        foreach ($bitLengths as $bitLength) {
-            $testData = array_merge(
-                $testData,
-                [
-                    [$smlPrime, $smlPrimeInverses[$bitLength], 0, $bitLength, 1],
-                    [$smlPrime, $smlPrimeInverses[$bitLength], 0, $bitLength, $bitLength],
-                    [$smlPrime, $smlPrimeInverses[$bitLength], $randXor, $bitLength, 1],
-                    [$smlPrime, $smlPrimeInverses[$bitLength], $randXor, $bitLength, $bitLength],
-                    [$lrgPrimes[$bitLength], $lrgPrimeInverses[$bitLength], 0, $bitLength, 1],
-                    [$lrgPrimes[$bitLength], $lrgPrimeInverses[$bitLength], 0, $bitLength, $bitLength],
-                    [$lrgPrimes[$bitLength], $lrgPrimeInverses[$bitLength], $randXor, $bitLength, 1],
-                    [$lrgPrimes[$bitLength], $lrgPrimeInverses[$bitLength], $randXor, $bitLength, $bitLength],
-                ]
-            );
-        }
-
-        return $testData;
-    }
-
-    /**
      * @dataProvider getBitLengthTestData
      */
     public function testEncodeDecodeRandomNumbers(int $bitLength)
@@ -140,6 +50,7 @@ class OptimusTest extends TestCase
                 $decoded,
                 "Encoded value $encoded has not decoded back to $id. ($assertMsgDetails)"
             );
+
             $this->assertNotEquals(
                 $id,
                 $encoded,
