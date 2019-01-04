d243505f186be0e236bc36d22633fb8d4135de4f
Jens Segers <segers.jens@gmail.com>
2019-01-04 12:34:57 +0100

    :white_check_mark: Add tests for gmp

--- tests/OptimusTest.php
+++ tests/OptimusTest.php
@@ -59,6 +59,46 @@ class OptimusTest extends TestCase
         ];
     }
 
+    /**
+     * @dataProvider getBitLengthTestData
+     */
+    public function testEncodeDecodeRandomNumbersWithGmp(int $bitLength)
+    {
+        $maxInt = (2 ** $bitLength) - 1;
+        list($prime, $inverse, $xor) = Energon::generate(null, $bitLength);
+
+        $optimus = new Optimus($prime, $inverse, $xor, $bitLength);
+        $optimus->setMode(Optimus::MODE_GMP);
+
+        for ($i = 0; $i < 1000; $i++) {
+            $id = random_int(0, $maxInt);
+
+            $encoded = $optimus->encode($id);
+            $decoded = $optimus->decode($encoded);
+
+            $assertMsgDetails = sprintf(
+                'Prime: %s, Inverse: %s, Xor: %s, Bit length: %s, Value: %s',
+                $prime,
+                $inverse,
+                $xor,
+                $bitLength,
+                $id
+            );
+
+            $this->assertEquals(
+                $id,
+                $decoded,
+                "Encoded value $encoded has not decoded back to $id. ($assertMsgDetails)"
+            );
+
+            $this->assertNotEquals(
+                $id,
+                $encoded,
+                "Encoded value $encoded matches the original value."
+            );
+        }
+    }
+
     public function testEncodeStrings()
     {
         list($prime, $inverse, $xor) = Energon::generate();
